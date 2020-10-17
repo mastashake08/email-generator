28163e81554a1b7180abd7985d4eca0aa0093c51
mastashake08 <jyrone.parker@gmail.com>
2020-10-17 02:40:56 -0400

    create emails with Mail-In-A-Box email

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/create-email', function(Request $request){
+  $endpoint = env("MIAB_HOST").'admin/mail/aliases/add';
+$client = new \GuzzleHttp\Client();
+$address = Str::random(8)."@".env('EMAIL_DOMAIN');
+$forward = $request->forward_to;
+
+$response = $client->request('POST', $endpoint, ['query' => [
+    'address' => $address,
+    'forwards_to' => $forward,
+]]);
+
+// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
+
+$statusCode = $response->getStatusCode();
+//$content = $response->getBody();
+
+// or when your server returns json
+ $content = json_decode($response->getBody(), true);
+ return $content;
 });
